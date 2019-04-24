c354ca66cb1a75e39a3f4f40fe8dcb62b9dc05f9
Sam Shi <samshi@DF-586184.local>
2019-04-24 17:01:24 -0400

    training gets Killed: 9

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,56 +1,237 @@
+import os
+import gc
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 from clean_session import preprocess_sessions
 from session_features import compute_session_fts
+from hotel2vec import hotel2vec
 from manual_encoding import action_encoding, click_view_encoding, meta_encoding
-from utils import load_data, Fprint
+from utils import load_data, Fprint, check_dir, check_gpu
+
+from sklearn.model_selection import StratifiedKFold
+import catboost as cat
 
 
-def pipeline(data_source='train', nrows=None):
+# def pipeline(data_source='train', nrows=None):
+#     fprint = Fprint().fprint
+#     fprint(f'Start data processing pipeline, first load raw {data_source} data')
+#     df = load_data(data_source, nrows=nrows)
+#
+#     fprint('Preprocessing data')
+#     # clip sessions to last clickout and remove sessions with no clickout ( and remove duplicates)
+#     df = preprocess_sessions(df, data_source=data_source, rd=True, fprint=fprint)
+#
+#     fprint('Compute session features')
+#     _ = compute_session_fts(df, data_source=data_source)
+#
+#     fprint('Getting manual encoding')
+#     fprint('Action encoding')
+#     _ = action_encoding()
+#     fprint('Click view encoding')
+#     _ = click_view_encoding()
+#     fprint('Meta encoding')
+#     _ = meta_encoding()
+#     fprint('Done manual encodings')
+
+
+def explode(df):
+    impressions = df['impressions'].values.tolist()
+    prices = df['prices'].values.tolist()
+
+    rs = [len(r) for r in impressions]
+    # locations
+    inds = np.concatenate([np.arange(i, dtype=int) for i in rs])
+    # relative locations
+    rel_inds = np.concatenate([np.arange(i) / i for i in rs])
+
+    # the rest cols
+    rest_cols = [c for c in df.columns if c not in ['impressions', 'prices']]
+    rest_arr = np.repeat(df[rest_cols].values, rs, axis=0)
+    # create dataframe to host the exploded
+    exploded = pd.DataFrame(np.column_stack((rest_arr, np.concatenate(impressions), np.concatenate(prices))),
+                            columns=rest_cols + ['impression', 'price'])
+    exploded['price'] = exploded['price'].astype(int)
+    exploded['impression_loc'] = inds
+    exploded['rel_impression_loc'] = rel_inds
+    exploded['impression'] = exploded['impression'].astype(int)
+    exploded['reference'] = exploded['reference'].astype(int)
+    exploded['step'] = exploded['step'].astype(int)
+
+    return exploded
+
+
+def compute_diff(df, grp, cols):
+    diff = (df.set_index('session_id')[cols] - grp[cols].mean()).reset_index(drop=True)
+    diff.columns = [f'{c}_diff' for c in diff.columns]
+    return pd.concat([df, diff], axis=1)
+
+
+
+def combine_inputs(data_source='train', nrows=None):
     fprint = Fprint().fprint
     fprint(f'Start data processing pipeline, first load raw {data_source} data')
-    df = load_data(data_source, nrows=nrows)
 
-    fprint('Preprocessing data')
-    # clip sessions to last clickout and remove sessions with no clickout ( and remove duplicates)
-    df = preprocess_sessions(df, data_source=data_source, rd=True, fprint=fprint)
+    filepath = './cache'
+    check_dir(filepath)
+    filename = os.path.join(filepath, 'combined_inputs.h5')
+    if os.path.isfile(filename):
+        store = pd.HDFStore(filename)
+        if data_source in store.keys():
+            fprint(f'Load {data_source} from existing {filename}')
+            session_fts = pd.read_hdf(filename, data_source)
+            return session_fts
 
-    fprint('Compute session features')
-    _ = compute_session_fts(df, data_source=data_source)
+    df = load_data('train', nrows=nrows)
+    df = preprocess_sessions(df, data_source='train', rd=True)
+    drop_cols = ['user_id', 'timestamp', 'current_filters']
+    df = df.drop(drop_cols, axis=1)
+    df = df.groupby('session_id').last().reset_index()
 
-    fprint('Getting manual encoding')
-    fprint('Action encoding')
-    _ = action_encoding()
-    fprint('Click view encoding')
-    _ = click_view_encoding()
-    fprint('Meta encoding')
-    _ = meta_encoding()
-    fprint('Done manual encodings')
+    # split to list
+    df['impressions'] = df.impressions.str.split('|')
+    df['prices'] = df.prices.str.split('|')
+
+    fprint('Exploding on impressions and prices')
+    df = explode(df)
+    fprint(f'After exploding, shape: {df.shape}')
+
+    # 1) all the manual encodings
+    ae = action_encoding()
+    ae_cols = [c for c in ae.columns if c != 'reference']
+    df = pd.merge(df.set_index('impression'), ae.set_index('reference'), left_index=True, right_index=True)
+    del ae
+    gc.collect()
+    df.index.name = 'impression'
+    df.reset_index(inplace=True)
+    fprint(f'After adding action encodings, shape: {df.shape}')
+
+    # 2) the hotel2vec encodings
+    hv = hotel2vec()
+    hv_cols = [c for c in hv.columns if c != 'item_id']
+    df = pd.merge(df.set_index('impression'), hv.set_index('item_id'), left_index=True, right_index=True)
+    del hv
+    gc.collect()
+    df.index.name = 'impression'
+    df.reset_index(inplace=True)
+    fprint(f'After adding hotelvec, shape: {df.shape}')
+
+    # 3) click view
+    cv = click_view_encoding()
+    cv_cols = [c for c in cv.columns if c != 'item_id']
+    df = pd.merge(df.set_index('impression'), cv.set_index('item_id'), left_index=True, right_index=True)
+    del cv
+    df.index.name = 'impression'
+    df.reset_index(inplace=True)
+    fprint(f'After adding clickview, shape: {df.shape}')
+
+    # 4) meta
+    meta = meta_encoding()
+    meta_cols = [c for c in meta.columns if c != 'item_id']
+    df = pd.merge(df.set_index('impression'), meta.set_index('item_id'), left_index=True, right_index=True)
+    del meta
+    df.index.name = 'impression'
+    df.reset_index(inplace=True)
+    fprint(f'After adding meta, shape: {df.shape}')
+
+    # groupby
+    grp = df.groupby('session_id')
+    # compute the relative difference
+    df = compute_diff(df, grp, ['price'])
+    df = compute_diff(df, grp, ae_cols)
+    df = compute_diff(df, grp, hv_cols)
+    df = compute_diff(df, grp, cv_cols)
+    df = compute_diff(df, grp, meta_cols)
+
+    fprint(f'Done combing data, shape: {df.shape}')
+    df.to_hdf(filename, data_source)
+    fprint(f'Done saving {filename}')
+
+    return df
 
 
+# encode city, platform and device
+def categorize(df, cols):
+    for col in cols:
+        print('converting', col)
+        unique_values = df[col].unique()
+        mapping = {v: k for k, v in enumerate(unique_values)}
+        df[col] = df[col].map(mapping)
 
 
+def create_model_inputs(df=None):
+    if df is None:
+        pass
+    else:
+        # create target
+        df['target'] = (df.reference == df.impression).astype(int)
+        del df['reference']
+        print(df['target'].value_counts())
+        # categorize
+        cat_fts = ['city', 'platform', 'device', 'action_type', 'impression']
+        categorize(df, cat_fts)
 
+        skf = StratifiedKFold(n_splits=5)
 
+        sids = df.session_id.values
+        target = df.target.values
+        del df['target']
 
+        device = 'GPU' if check_gpu() else 'CPU'
+        params = {'iterations': 1000,
+                  'learning_rate': 0.02,
+                  'depth': 8,
+                  'task_type': device,
+                  'loss_function': 'MultiClass',
+                  'eval_metric': 'Accuracy'}
+        for trn_ind, val_ind in skf.split(sids, sids):
+            trn_mask = df.session_id.isin(sids[trn_ind])
+            del df['session_id']
+            x_trn, x_val = df[trn_mask], df[~trn_mask]
+            y_trn, y_val = target[trn_mask], target[~trn_mask]
 
+            categorical_ind = [k for k, v in enumerate(x_trn.columns) if v in cat_fts]
 
+            # train model
+            clf = cat.CatBoostClassifier(**params)
+            clf.fit(x_trn.values, y_trn,
+                    cat_features=categorical_ind,
+                    eval_set=(x_val.values, y_val),
+                    early_stopping_rounds=100,
+                    verbose=100,
+                    plot=False)
+            print('Done!')
+            print('Grab feature importance for both train and val')
+            # get feature importance
+            trn_imp = clf.get_feature_importance(data=cat.Pool(data=x_trn, cat_features=categorical_ind),
+                                                 prettified=True)
+            val_imp = clf.get_feature_importance(data=cat.Pool(data=x_val, cat_features=categorical_ind),
+                                                 prettified=True)
+            plot_imp(trn_imp, 'train')
+            plot_imp(val_imp, 'val')
+            print('Done feature imp')
+            break
 
+def plot_imp(data, fold_, plot_n=15):
+    check_dir('./imps')
+    imp = pd.DataFrame.from_records(data)
+    imp.to_csv(f'./imps/{fold_}.csv', index=False)
+    imp.columns = ['features', 'feature_importance']
+    imp_des = imp.sort_values(by='feature_importance', ascending=False)
+    imp_asc = imp.sort_values(by='feature_importance', ascending=True)
 
-
-
-
-
-
-
-
-
-
-    fprint('Done data pipeline')
+    fig, axes = plt.subplots(figsize=(8, 8), nrows=2, ncols=1)
+    imp_des[:plot_n].plot(x='features', y='feature_importance', ax=axes[0], kind='barh', grid=True)
+    imp_asc[:plot_n].plot(x='features', y='feature_importance', ax=axes[1], kind='barh', grid=True)
+    plt.tight_layout()
+    fig.savefig('./imps/{}.png'.format(fold_))
 
 
 if __name__ == '__main__':
     data_source = 'train'
     # nrows = 10000
     nrows = None
-    pipeline(data_source, nrows=nrows)
-
-
+    # pipeline(data_source, nrows=nrows)
+    df = combine_inputs(data_source=data_source, nrows=nrows)
+    create_model_inputs(df)

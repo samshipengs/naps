8107b671f0a14d0ecf9e719c21b07f36c5be9f43
Sam Shi <samshi@DF-586184.local>
2019-05-06 09:56:10 -0400

    Draft input data creation

--- train_nn.py
+++ train_nn.py
@@ -3,7 +3,7 @@ import numpy as np
 import datetime, time, os, gc, re, sys
 from functools import partial
 import matplotlib.pyplot as plt
-from utils import ignore_warnings, load_data, get_logger
+from utils import ignore_warnings, load_data, get_logger, check_dir
 from clean_session import preprocess_sessions
 
 
@@ -69,85 +69,134 @@ def compute_session_fts(df):
 
 
 def create_inputs(nrows=100000):
-    logger.info('LOAD TRAIN')
-    train = load_train(nrows)
-    logger.info('COMPUTE SESSION FEATURES')
-    train = compute_session_fts(train)
+    filepath = './cache'
+    check_dir(filepath)
+    filename = os.path.join(filepath, 'processed_train.snappy')
+    if os.path.isfile(filename):
+        logger.info(f'LOAD FROM EXISTING {filename}')
+        train = pd.read_parquet(filename)
+    else:
+        logger.info('LOAD TRAIN')
+        train = load_train(nrows)
+        logger.info('COMPUTE SESSION FEATURES')
+        train = compute_session_fts(train)
 
-    logger.info('ONLY SELECT LAST CLICKOUT FROM EACH SESSION')
-    train = train.groupby('session_id').last().reset_index()
+        logger.info('ONLY SELECT LAST CLICKOUT FROM EACH SESSION')
+        train = train.groupby('session_id').last().reset_index()
 
-    logger.info('LOWER CASE CURRENT FILTERS AND SPLIT TO LIST')
-    train['cfs'] = train['current_filters_last_filters'].str.lower().str.split('|')
+        logger.info('LOWER CASE CURRENT FILTERS AND SPLIT TO LIST')
+        train['cfs'] = train['current_filters_last_filters'].str.lower().str.split('|')
 
-    logger.info('SPLIT PRICES STR TO LIST AND CONVERT TO INT')
-    train['prices'] = train['prices'].str.split('|')
-    train['prices'] = train['prices'].apply(lambda x: [int(p) for p in x])
-    logger.info('PAD 0S FOR LENGTH SHORTER THAN 25 (HMM)')
-    train['prices'] = train.prices.apply(lambda x: np.pad(x, (0, 25-len(x)), mode='constant'))
+        logger.info('SPLIT PRICES STR TO LIST AND CONVERT TO INT')
+        train['prices'] = train['prices'].str.split('|')
+        train['prices'] = train['prices'].apply(lambda x: [int(p) for p in x])
+        logger.info('PAD 0S FOR LENGTH SHORTER THAN 25 (HMM)')
+        train['prices'] = train.prices.apply(lambda x: np.pad(x, (0, 25-len(x)), mode='constant'))
 
-    logger.info('SPLIT IMPRESSION STR TO LIST OF IMPRESSIONS')
-    train['impressions'] = train['impressions'].str.split('|')
-    logger.info('CONVERT IMPRESSION STR TO INT')
-    train['impressions'] = train['impressions'].apply(lambda x: [int(i) for i in x])
-    logger.info('CONVERT REFERENCE ID TO INT')
-    train['reference'] = train['reference'].astype(int)
-    logger.info('PAD 0S FOR LENGTH SHORTER THAN 25 (HMM)')
-    train['impressions'] = train['impressions'].apply(lambda x: np.pad(x, (0, 25-len(x)), mode='constant'))
+        logger.info('SPLIT IMPRESSION STR TO LIST OF IMPRESSIONS')
+        train['impressions'] = train['impressions'].str.split('|')
+        logger.info('CONVERT IMPRESSION STR TO INT')
+        train['impressions'] = train['impressions'].apply(lambda x: [int(i) for i in x])
+        logger.info('CONVERT REFERENCE ID TO INT')
+        train['reference'] = train['reference'].astype(int)
+        logger.info('PAD 0S FOR LENGTH SHORTER THAN 25 (HMM)')
+        train['impressions'] = train['impressions'].apply(lambda x: np.pad(x, (0, 25-len(x)), mode='constant'))
 
-    logger.info('ASSIGN TARGET')
-    # filter out nan rows with reference_id not in impressions list, since if the true target in test
-    # is not in the impression list then it would not get evaluated
-    def assign_target(row):
-        ref = row['reference']
-        imp = list(row['impressions'])
-        if ref in imp:
-            return imp.index(ref)
-        else:
-            return np.nan
-
-    logger.info('ALSO ASSIGN LOCATION OF PRVIOUS REFERENCE ID')
-    def assign_last_ref_id(row):
-        ref = row['reference_last_reference_id']
-        imp = [str(i) for i in row['impressions']]
-        if pd.isna(ref):
-            return np.nan
-        else:
+        logger.info('ASSIGN TARGET')
+        # filter out nan rows with reference_id not in impressions list, since if the true target in test
+        # is not in the impression list then it would not get evaluated
+        def assign_target(row):
+            ref = row['reference']
+            imp = list(row['impressions'])
             if ref in imp:
                 return imp.index(ref)
             else:
                 return np.nan
 
-    train['target'] = train.apply(assign_target, axis=1)
-    train['last_ref_ind'] = train.apply(assign_last_ref_id, axis=1)
+        logger.info('ALSO ASSIGN LOCATION OF PRVIOUS REFERENCE ID')
+        def assign_last_ref_id(row):
+            ref = row['reference_last_reference_id']
+            imp = [str(i) for i in row['impressions']]
+            if pd.isna(ref):
+                return np.nan
+            else:
+                if ref in imp:
+                    return imp.index(ref)
+                else:
+                    return np.nan
 
-    logger.info('REMOVE THE ONES WHOSE REFERENCE ID IS NOT IN IMPRESSION LIST')
-    # drop the ones whose reference is not in the impression list
-    train = train[train['target'].notna()].reset_index(drop=True)
-    train['target'] = train['target'].astype(int)
+        train['target'] = train.apply(assign_target, axis=1)
+        train['last_ref_ind'] = train.apply(assign_last_ref_id, axis=1)
 
-    logger.info(f"Look at target distribution: \n{pd.value_counts(train['target']).head()}")
+        logger.info('REMOVE THE ONES WHOSE REFERENCE ID IS NOT IN IMPRESSION LIST')
+        # drop the ones whose reference is not in the impression list
+        train = train[train['target'].notna()].reset_index(drop=True)
+        train['target'] = train['target'].astype(int)
 
-    # create meta ohe
-    logger.info('LOAD META DATA')
-    meta_df = load_data('item_metadata')
-    logger.info('LOWER AND SPLIT PROPERTIES STR TO LIST')
-    meta_df['properties'] = meta_df['properties'].str.lower().str.split('|')
-    logger.info('GET ALL UNIQUE PROPERTIES')
-    unique_properties = list(set(np.concatenate(meta_df['properties'].values)))
-    property2natural = {v: k for k, v in enumerate(unique_properties)}
-    n_properties = len(unique_properties)
-    logger.info(f'TOTAL NUMBER OF UNIQUE META PROPERTIES: {n_properties}')
-    logger.info('CONVERT THE PROPERTIES TO OHE AND SUPERPOSE FOR EACH ITEM_ID')
-    meta_df['properties'] = meta_df['properties'].apply(lambda ps: [property2natural[p] for p in ps])
-    meta_df['properties'] = meta_df['properties'].apply(lambda ps: np.sum(np.eye(n_properties, dtype=int)[ps], axis=0))
-    logger.info('CREATE MAPPINGS')
-    meta_mapping = dict(meta_df[['item_id', 'properties']].values)
-    # add a mapping for the padded values
-    meta_mapping[0] = np.zeros(n_properties, dtype=int)
-    del meta_df, unique_properties, property2natural
-    gc.collect()
+        logger.info(f"Look at target distribution: \n{pd.value_counts(train['target']).head()}")
 
-    logger.info('')
-    logger.info('')
-    logger.info('')
+        # create meta ohe
+        logger.info('LOAD META DATA')
+        meta_df = load_data('item_metadata')
+        logger.info('LOWER AND SPLIT PROPERTIES STR TO LIST')
+        meta_df['properties'] = meta_df['properties'].str.lower().str.split('|')
+        logger.info('GET ALL UNIQUE PROPERTIES')
+        unique_properties = list(set(np.concatenate(meta_df['properties'].values)))
+        property2natural = {v: k for k, v in enumerate(unique_properties)}
+        n_properties = len(unique_properties)
+        logger.info(f'TOTAL NUMBER OF UNIQUE META PROPERTIES: {n_properties}')
+        logger.info('CONVERT THE PROPERTIES TO OHE AND SUPERPOSE FOR EACH ITEM_ID')
+        meta_df['properties'] = meta_df['properties'].apply(lambda ps: [property2natural[p] for p in ps])
+        meta_df['properties'] = meta_df['properties'].apply(lambda ps: np.sum(np.eye(n_properties, dtype=int)[ps], axis=0))
+        logger.info('CREATE MAPPINGS')
+        meta_mapping = dict(meta_df[['item_id', 'properties']].values)
+        # add a mapping for the padded values
+        meta_mapping[0] = np.zeros(n_properties, dtype=int)
+        del meta_df, unique_properties, property2natural
+        gc.collect()
+
+        logger.info('APPLY META OHE MAPPING TO IMPRESSIONS (THIS COULD TAKE SOME TIME)')
+        train['impressions'] = (train['impressions']
+                                .apply(lambda imps: np.vstack([meta_mapping[i]
+                                                               if i in meta_mapping.keys()
+                                                               else np.zeros(n_properties, dtype=int)
+                                                               for i in imps])))
+        del meta_mapping
+        gc.collect()
+
+        logger.info('CREATE OHE SUPERPOSITION OF FILTERS')
+        unique_cfs = list(set(np.concatenate(train['cfs'].dropna().values)))
+        cfs_mapping = {v: k for k, v in enumerate(unique_cfs)}
+        n_cfs = len(unique_cfs)
+        logger.info(f'THERE ARE TOTAL {n_cfs} UNIQUE FILTERS')
+
+        logger.info('APPLY FILTERS OHE SUPERPOSITION TO EACH RECORDS')
+        train.loc[train['cfs'].notna(), 'cfs'] = (train.loc[train['cfs'].notna(), 'cfs']
+                                                  .apply(lambda cfs: [cfs_mapping[cf] for cf in cfs]))
+        # zeros if the cfs is nan (checked with type(cfs) is list not float
+        train['cfs'] = (train['cfs'].apply(lambda cfs: np.sum(np.eye(n_cfs, dtype=int)[cfs], axis=0)
+                                                if type(cfs) == list else np.zeros(n_cfs, dtype=int)))
+        del cfs_mapping
+        gc.collect()
+
+        logger.info('NORMALIZE PRICES')
+        # maybe normalize to percentage within each records, check does each item_id have the same price over all records
+        def normalize(ps):
+            p_arr = np.array(ps)
+            return p_arr / (p_arr.max())
+        train['prices'] = train['prices'].apply(normalize)
+        logger.info(f'SAVE {filename}')
+        train.to_parquet(filename)
+        logger.info('DONE SAVING')
+    return train
+
+
+def pipeline():
+    train = create_inputs(nrows=100000)
+
+
+
+
+
+if __name__ == '__main__':
+    pipeline()
